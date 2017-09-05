fff6f291738c7964887680f6ee25b76a861c220e
Yiting <yiting@Yitings-MacBook.local>
2017-09-05 01:37:33 -0400

    add function

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+// input underline animation when focus
 $(".input_group input").focus(function(){
     $(this).parent().next().addClass("input_highlight")
 });
@@ -6,9 +7,20 @@ $(".input_group input").focusout(function(){
     $(this).parent().next().removeClass("input_highlight")
 })
 
+//select category, then send to input value
 $(".category button").click(function(){
     $(this).parent().addClass("selected");
     $(this).parent().siblings().removeClass("selected");
 
     $("#category_input").val($(this).find("p").text());
 })
+
+// quantity label animation
+$(".input_group #quantity_input").focus(function(){
+    $(this).next().addClass("small");
+});
+
+$(".input_group #quantity_input").focusout(function(){
+    if($(this).val()=='') $(this).next().removeClass("small");
+});
+
